0a3b3050d5db9c8fb4ec47d426148639e832bd3e
ialoig <ialoig@protonmail.com>
2017-03-05 20:54:59 +0100

    Added Pygments & Stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-
+	class HTMLwithPygments < Redcarpet::Render::HTML
+		def block_code (code, language)
+			Pygments.highlight(code, lexer: language)
+		end
+	end
 	def markdown (content)
-		render = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+		render = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
 		option = {
 			autolink: true,
 			no_intra_emphasis: true,
