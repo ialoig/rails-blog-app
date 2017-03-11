d9ec39afdab0eb73b4ed594056cd796c91506714
ialoig <ialoig@protonmail.com>
2017-03-11 19:20:44 +0100

    Set friendly_id for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,7 +22,7 @@ class ProjectsController < ApplicationController
 	end
 
 	private def find_project
-		@project = Project.find(params[:id])
+		@project = Project.friendly.find(params[:id])
 	end
 
 	private def project_params
