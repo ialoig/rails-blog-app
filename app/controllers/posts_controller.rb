4768b10baa19d3ff7f5aeb59e1105843026368cb
ialoig <ialoig@protonmail.com>
2017-03-11 16:41:28 +0100

    Add friendly_id gem and setup friendly_id on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,11 +37,11 @@ class PostsController < ApplicationController
 	end
 
 	private def post_params
-		params.require(:post).permit(:title, :content)
+		params.require(:post).permit(:title, :content, :slug)
 	end
 
 	private def find_post
-		@post = Post.find(params[:id])
+		@post = Post.friendly.find(params[:id])
 	end
 
 end
