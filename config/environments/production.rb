e3804b4c8b501638af9e3558756efd9de06ccb70
ialoig <ialoig@protonmail.com>
2017-03-26 20:25:44 +0200

    Updated environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,9 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.sendgrid.net",
     :port                 => "587",
-    :user_name            => "apikey",
-    :password             => ENV["SENDGRID_API_KEY"],
+    :authentication       => "plain",
+    :user_name            => ENV["SENDGRID_USERNAME"],
+    :password             => ENV["SENDGRID_PASSWORD"],
     :domain               => "heroku.com",
     :enable_starttls_auto => true
   }
